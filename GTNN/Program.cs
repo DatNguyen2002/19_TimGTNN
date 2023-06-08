773adbd45c7a0fbbd624d15f98890033e9ce4cff
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-06-08 15:05:43 +0700

    Update Program.cs

--- GTNN/Program.cs
+++ GTNN/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace GTNN
 {
+
     internal class Program
     {
-        static void Main(string[] args)
+        public static int MinValue(int[] array)
         {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 56, 1, 5, 10 };
+            int index = MinValue(array);
+            Console.WriteLine(array[index]);
+
+
+            int[] mang = { 5, 3, 9, 10, 7 };
+            int giaTriNhoNhat = mang.Min();
+            Console.WriteLine("Giá trị nhỏ nhất trong mảng là: " + giaTriNhoNhat);
+            Console.ReadKey();
         }
     }
 }
